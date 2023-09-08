4edc83df7008aa0450a53f272bf72d0e2166a6e5
Junwoo-Seo <junwoo.seo.1998@gmail.com>
2023-09-07 18:40:34 -0700

    added logger - need git submodule update!!!!

---
+++ HostileEngine/Libs/spdlog/premake5.lua
@@ -0,0 +1,44 @@
+project "spdlog"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "c++17"
+    staticruntime "on"
+    
+    --targetdir ("bin/".."%{prj.name}")
+    --objdir ("bin-int/".."%{prj.name}")
+
+    disablewarnings {
+        "4819",
+    }
+
+    includedirs
+    {
+        "spdlog/include",
+    }
+
+    files
+    {
+        "spdlog/src/**.cpp",
+        "spdlog/src/**.h",
+        "spdlog/include/**.h",
+    }
+    defines
+    {
+        "_CRT_SECURE_NO_WARNINGS",
+        "SPDLOG_COMPILED_LIB"
+    }
+    filter "system:windows"
+        flags
+        {
+            "MultiProcessorCompile"
+        }
+
+        systemversion "latest"
+    
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
