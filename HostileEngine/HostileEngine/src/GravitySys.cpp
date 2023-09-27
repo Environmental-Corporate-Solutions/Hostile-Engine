b7c9fbc7fad590d24e243dd52d7d514d705bcb68
beongkyuPark <byeonggyu.park.kr@gmail.com>
2023-09-27 12:12:17 -0700

    spheres are working again

--- HostileEngine/HostileEngine/src/GravitySys.cpp
+++ HostileEngine/HostileEngine/src/GravitySys.cpp
@@ -13,6 +13,7 @@
 #include "TransformSys.h"
 #include "DetectCollisionSys.h"
 #include "GraphicsSystem.h"//Mesh
+#include "Rigidbody.h"//tag
 
 namespace Hostile {
 
@@ -40,18 +41,19 @@ namespace Hostile {
                 set<MassProperties>({ Mass }).
                 set<Transform>({ {-18.f,2.f,-18.f},{Quaternion::CreateFromAxisAngle(Vector3::UnitY, 0.f) } }).
                 set<InertiaTensor>({ {inertiaTensor.Inverse()}, {} }).
-                add<Mesh>().set<Mesh>({ "Cube", 0 });
+                add<Mesh>().set<Mesh>({ "Cube", 0 }).
+                add<Rigidbody>();
 
-			e1 = _world.entity();
-			e1.set_name("Sphere2").
-				set<SphereCollider>({ Rad }).
-				set<Velocity>({ {0,0,0},{0,0,0} }).
-				add<Acceleration>().
-				add<Force>().
-				set<MassProperties>({ Mass }).
-				set<Transform>({ {-3.5f,15.f,-3.f},{Quaternion::CreateFromAxisAngle(Vector3::UnitY, 0.f) } }).
-				set<InertiaTensor>({ {inertiaTensor.Inverse()}, {} }).
-                add<Mesh>().set<Mesh>({ "Cube", 0 });
+			//e1 = _world.entity();
+			//e1.set_name("Sphere2").
+			//	set<SphereCollider>({ Rad }).
+			//	set<Velocity>({ {0,0,0},{0,0,0} }).
+			//	add<Acceleration>().
+			//	add<Force>().
+			//	set<MassProperties>({ Mass }).
+			//	set<Transform>({ {-3.5f,15.f,-3.f},{Quaternion::CreateFromAxisAngle(Vector3::UnitY, 0.f) } }).
+			//	set<InertiaTensor>({ {inertiaTensor.Inverse()}, {} }).
+   //             add<Mesh>().set<Mesh>({ "Cube", 0 });
 
             ////2. box
             //auto e2 = _world.entity();
