5210bd5e25ed738d6a1b91a8db8908c51852b2b7
beongkyuPark <byeonggyu.park.kr@gmail.com>
2023-10-19 02:33:06 -0700

    set physics phases update rate to 60 FPS (capped at)

--- HostileEngine/HostileEngine/src/GravitySys.cpp
+++ HostileEngine/HostileEngine/src/GravitySys.cpp
@@ -35,12 +35,12 @@ namespace Hostile {
 
             auto e1 = _world.entity("box1");
             e1.add<BoxCollider>().//tag
-                set<Velocity>({ {0,0,29.5},{0,0,0} }).
-                set<Acceleration>({ { 0,0,0 }, {0,0,0} }).
+                set<Velocity>({ {0,13.7,-30},{0,1,0.1} }).
+                set<Acceleration>({ {0,0,0}, {0,0,0} }).
                 add<Force>().
                 set<MassProperties>({ Mass }).
                 set<Transform>({
-                    {-2.f,1.2f,-50.f},
+                    {-2.2f,1.f,85.f},
                     {Quaternion::CreateFromAxisAngle(Vector3::UnitY, 0.f) },
                     {Scl, Scl, Scl} }).
                     set<InertiaTensor>({ {inertiaTensor.Inverse()}, {} }).
@@ -107,7 +107,7 @@ namespace Hostile {
                 set<Acceleration>({ { 0,0,0 }, { 0,0,0 } }).
                 add<Force>().
                 set<MassProperties>({ Mass }).
-                set<Transform>({ {-2.5f,80.f,-2.5f},
+                set<Transform>({ {-2.5f,55.f,-2.5f},
                     {Quaternion::CreateFromAxisAngle(Vector3::UnitY, 0.f) },
                     {Scl,Scl,Scl}
                     }).
@@ -120,7 +120,7 @@ namespace Hostile {
                 set<Acceleration>({ { 0,0,0 }, { 0,0,0 } }).
                 add<Force>().
                 set<MassProperties>({ Mass }).
-                set<Transform>({ {-1.f,80.f,-1.5f},
+                set<Transform>({ {-1.f,45.f,-1.5f},
                     {Quaternion::CreateFromAxisAngle(Vector3::UnitY, 0.f) },
                     {Scl,Scl,Scl}
                     }).
@@ -137,10 +137,6 @@ namespace Hostile {
     }
 
     void GravitySys::OnUpdate(flecs::iter& _it, Force* force, MassProperties* mass) {
-        //delete collisionData
-        IEngine::Get().GetWorld().each<CollisionData>([](flecs::entity e, CollisionData& cd) {
-            e.remove<CollisionData>();
-            });
 
         const Vector3 GravitationalAcc = _it.world().get<Gravity>()->direction;
         const size_t Count = _it.count();
